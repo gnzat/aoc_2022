9723c2d07b57fe9a45dbe3a441fc00b4b3d29446
gnzat <100507662+gnzat@users.noreply.github.com>
2023-05-23 16:21:39 +0800

    feat: add day4 (#7)

---
+++ day_4/Cleanup.js
@@ -0,0 +1,39 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("fs");
+function readFile(file) {
+    var data = fs.readFileSync(file, "utf8");
+    var content = data.split("\n");
+    return content;
+}
+function isWithin(line) {
+    var index = line.indexOf(",");
+    var first = line.slice(0, index);
+    var second = line.slice(index + 1);
+    //   console.log(first, second);
+    var arr1 = first.split("-");
+    var arr2 = second.split("-");
+    //   console.log(arr1, arr2);
+    var a1 = Number(arr1[0]);
+    var b1 = Number(arr1[1]);
+    var a2 = Number(arr2[0]);
+    var b2 = Number(arr2[1]);
+    if (a1 >= a2 && b1 <= b2 && a1 <= b2 && b1 >= a2) {
+        return true;
+    }
+    else if (a2 >= a1 && b2 <= b1 && a2 <= b1 && b2 >= a1) {
+        return true;
+    }
+    return false;
+}
+// const arr = readFile("input_test.txt");
+var arr = readFile("input.txt");
+// console.log(arr);
+var total = 0;
+for (var i = 0; i < arr.length; i++) {
+    if (isWithin(arr[i])) {
+        total += 1;
+    }
+}
+console.log(total);
+// 448
---
+++ day_4/Cleanup2.ts
@@ -0,0 +1,46 @@
+import * as fs from "fs";
+
+function readFile(file: string) {
+  const data = fs.readFileSync(file, "utf8");
+  const content = data.split("\n");
+  return content;
+}
+
+function overlap(line: string): boolean {
+  const index = line.indexOf(",");
+  const first = line.slice(0, index);
+  const second = line.slice(index + 1);
+  //   console.log(first, second);
+
+  const arr1 = first.split("-");
+  const arr2 = second.split("-");
+  //   console.log(arr1, arr2);
+
+  const a1 = Number(arr1[0]);
+  const b1 = Number(arr1[1]);
+  const a2 = Number(arr2[0]);
+  const b2 = Number(arr2[1]);
+
+  if (a1 >= a2 && b1 <= b2 && a1 <= b2 && b1 >= a2) {
+    return true;
+  } else if (a2 >= a1 && b2 <= b1 && a2 <= b1 && b2 >= a1) {
+    return true;
+  } else if ((a1 < a2 && b1 >= a2) || (a2 < a1 && b2 >= a1)) {
+    return true;
+  }
+  return false;
+}
+
+// const arr = readFile("input_test.txt");
+const arr = readFile("input.txt");
+// console.log(arr);
+let total = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  if (overlap(arr[i])) {
+    total += 1;
+  }
+}
+
+console.log(total);
+// 794
