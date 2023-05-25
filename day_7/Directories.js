e4eb547d86be1866d609c439d910b217dba9133c
gnzat <tazngmt@gmail.com>
2023-05-25 15:38:16 +0800

    feat: add day7

---
+++ day_7/Directories.js
@@ -0,0 +1,70 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("fs");
+// const file = "input_test.txt";
+var file = "input.txt";
+var data = fs.readFileSync(file, "utf8");
+var lines = data.split("\n");
+var cd = ["/"];
+var sizes = { "/": 0 };
+for (var _i = 0, lines_1 = lines; _i < lines_1.length; _i++) {
+    var line = lines_1[_i];
+    var l = line.split(" "); //split line into array
+    if (l[0] === "$") {
+        //handle paths
+        if (l[1] === "cd") {
+            if (l[2] === "/") {
+                cd = ["/"];
+            }
+            else if (l[2] === "..") {
+                cd.pop();
+            }
+            else {
+                cd.push(l[2]);
+            }
+        }
+        //handle lists:
+        if (l[1] === "ls") {
+            continue;
+        }
+    }
+    else if (l[0] === "dir") {
+        var path = cd.join("") + l[1];
+        sizes[path] = 0;
+    }
+    //if it is a number (file size)
+    else if (!isNaN(Number(l[0]))) {
+        // console.log(l[0]);
+        var path = cd.join("");
+        sizes[path] += Number(l[0]);
+        //to add file sizes to all directories involved:
+        for (var i = 1; i < cd.length; i++) {
+            var new_cd = cd.slice(0, -i).join("");
+            // console.log(new_cd);
+            sizes[new_cd] += Number(l[0]);
+        }
+    }
+}
+// console.log(sizes);
+// let total: number = 0;
+// for (const key of Object.keys(sizes)) {
+//     //part 1:
+//     if (sizes[key] <= 100000) {
+//         // console.log(sizes[key]);
+//         total += sizes[key];
+//     }
+// }
+// console.log(total);
+// //2031851
+//part 2:
+var unused = 70000000 - sizes["/"];
+var need = 30000000 - unused;
+var min = sizes["/"];
+for (var _a = 0, _b = Object.keys(sizes); _a < _b.length; _a++) {
+    var key = _b[_a];
+    if (sizes[key] >= need && sizes[key] < min) {
+        min = sizes[key];
+    }
+}
+console.log(min);
+//2568781
---
+++ day_7/Directories.ts
@@ -0,0 +1,73 @@
+import * as fs from "fs";
+
+// const file = "input_test.txt";
+const file = "input.txt";
+const data = fs.readFileSync(file, "utf8");
+const lines: string[] = data.split("\n");
+
+let cd = ["/"];
+let sizes = { "/": 0 };
+for (const line of lines) {
+  let l = line.split(" "); //split line into array
+
+  if (l[0] === "$") {
+    //handle paths
+    if (l[1] === "cd") {
+      if (l[2] === "/") {
+        cd = ["/"];
+      } else if (l[2] === "..") {
+        cd.pop();
+      } else {
+        cd.push(l[2]);
+      }
+    }
+
+    //handle lists:
+    if (l[1] === "ls") {
+      continue;
+    }
+  } else if (l[0] === "dir") {
+    const path: string = cd.join("") + l[1];
+    sizes[path] = 0;
+  }
+
+  //if it is a number (file size)
+  else if (!isNaN(Number(l[0]))) {
+    // console.log(l[0]);
+    const path: string = cd.join("");
+    sizes[path] += Number(l[0]);
+
+    //to add file sizes to all directories involved:
+    for (let i = 1; i < cd.length; i++) {
+      const new_cd: string = cd.slice(0, -i).join("");
+      // console.log(new_cd);
+      sizes[new_cd] += Number(l[0]);
+    }
+  }
+}
+// console.log(sizes);
+
+// let total: number = 0;
+// for (const key of Object.keys(sizes)) {
+
+//     //part 1:
+//     if (sizes[key] <= 100000) {
+//         // console.log(sizes[key]);
+//         total += sizes[key];
+//     }
+// }
+// console.log(total);
+// //2031851
+
+//part 2:
+const unused: number = 70000000 - sizes["/"];
+const need: number = 30000000 - unused;
+let min: number = sizes["/"];
+
+for (const key of Object.keys(sizes)) {
+  if (sizes[key] >= need && sizes[key] < min) {
+    min = sizes[key];
+  }
+}
+console.log(min);
+//2568781
