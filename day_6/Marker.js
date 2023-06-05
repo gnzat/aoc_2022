002cf254f9b822114b67965a92c7f91e197868ff
gnzat <tazngmt@gmail.com>
2023-06-05 13:51:25 +0800

    feat: changed formatting for day6

--- day_6/Marker.js
+++ day_6/Marker.js
@@ -4,12 +4,11 @@ var fs = require("fs");
 // const file = "input_test.txt";
 var file = "input.txt";
 var content = fs.readFileSync(file, "utf8");
+// part 1: replace "13" with "3"; "14" with "4"
 for (var start = 0; start < content.length - 13; start++) {
-    var letters = new Set;
+    var letters = new Set();
     for (var i = 0; i < 14; i++) {
         letters.add(content[start + i]);
-        // console.log(content[start + i]);
-        // console.log(letters);
     }
     if (letters.size !== 14) {
         continue;
@@ -20,3 +19,4 @@ for (var start = 0; start < content.length - 13; start++) {
     }
 }
 //1566
+//2265
--- day_6/Marker.ts
+++ day_6/Marker.ts
@@ -4,19 +4,17 @@ import * as fs from "fs";
 const file = "input.txt";
 const content = fs.readFileSync(file, "utf8");
 
+// part 1: replace "13" with "3"; "14" with "4"
 for (let start = 0; start < content.length - 13; start++) {
-  const letters = new Set;
+  const letters = new Set();
 
   for (let i = 0; i < 14; i++) {
     letters.add(content[start + i]);
-    // console.log(content[start + i]);
-    // console.log(letters);
   }
 
   if (letters.size !== 14) {
     continue;
-  }
-  else if (letters.size === 14) {
+  } else if (letters.size === 14) {
     console.log(start + 14);
     break;
   }
